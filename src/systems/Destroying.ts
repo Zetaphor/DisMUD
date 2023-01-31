39fbd5fcde691f592a166f6763e50c3498fd6faf
Zetaphor <zetaphor@hey.com>
2023-01-31 11:39:04 -0500

    Destroy entities that don't have drops

--- src/systems/Destroying.ts
+++ src/systems/Destroying.ts
@@ -6,19 +6,26 @@ const destroyingSystem = (world) => {
   const Destroyed = world._components["destroyed"];
   const DeathDrops = world._components["deathDrops"];
 
-  const destroyedQuery = defineQuery([Destroyed, DeathDrops]);
+  const destroyedDropsQuery = defineQuery([Destroyed, DeathDrops]);
+  const destroyedQuery = defineQuery([Destroyed]);
 
-  const ents = destroyedQuery(world);
+  // Destroy entities that have a drop defined
+  let ents = destroyedDropsQuery(world);
   for (let i = 0; i < ents.length; i++) {
     const eid = ents[i];
-
-    if (hasComponent(world, DeathDrops, eid)) {
-      for (let i = 0; i < DeathDrops.qty[eid]; i++) {
-        const dropDefinition = getDropByIndex(DeathDrops.dropIndex[eid]);
-        createEntity(world, dropDefinition.entityName, dropDefinition.data);
-        console.log("Dropped " + dropDefinition.description);
-      }
+    for (let i = 0; i < DeathDrops.qty[eid]; i++) {
+      const dropDefinition = getDropByIndex(DeathDrops.dropIndex[eid]);
+      createEntity(world, dropDefinition.entityName, dropDefinition.data);
+      console.log("Dropped " + dropDefinition.description);
     }
+    console.log(`Destroyed ${eid}`);
+    removeEntity(world, eid);
+  }
+
+  // Destroy entities that don't have a drop defined
+  ents = destroyedQuery(world);
+  for (let i = 0; i < ents.length; i++) {
+    const eid = ents[i];
     console.log(`Destroyed ${eid}`);
     removeEntity(world, eid);
   }
