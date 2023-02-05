9ba4345731ca75e16fcf15564e694e95ab607f14
Zetaphor <zetaphor@hey.com>
2023-02-05 20:16:28 -0300

    Cleanup

--- src/messages/room.ts
+++ src/messages/room.ts
@@ -1,5 +1,5 @@
 export const roomMessages = {
-  displayRoom: function (user, roomData) {
+  displayRoom(user, roomData) {
     user.send(`
     ☀️ **__${roomData.name}__**\n
       ${roomData.desc}
