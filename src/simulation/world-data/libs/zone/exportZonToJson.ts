26dbb2bc098f0fab7aa71eaf01d9a4f56905e56e
Zetaphor <zetaphor@hey.com>
2023-02-07 19:07:30 -0300

    Import zone data

--- src/simulation/world-data/libs/zone/exportZonToJson.ts
+++ src/simulation/world-data/libs/zone/exportZonToJson.ts
@@ -3,7 +3,36 @@ import parseZon from "./parseZon";
 
 const fs = require("fs");
 
-const zonNum = 9;
+// const zonNum = 0;
+// const zonNum = 9;
+// const zonNum = 12;
+// const zonNum = 15;
+// const zonNum = 25; // Issue
+// const zonNum = 30;
+// const zonNum = 31;
+// const zonNum = 33;
+// const zonNum = 35;
+// const zonNum = 36;
+// const zonNum = 40; // Issue
+// const zonNum = 50;
+// const zonNum = 51;
+// const zonNum = 52;
+// const zonNum = 53;
+// const zonNum = 54;
+// const zonNum = 60;
+// const zonNum = 61;
+// const zonNum = 62;
+// const zonNum = 63;
+// const zonNum = 64;
+// const zonNum = 65;
+// const zonNum = 70;
+// const zonNum = 71;
+// const zonNum = 72;
+// const zonNum = 73;
+// const zonNum = 79;
+// const zonNum = 120;
+// const zonNum = 150;
+// const zonNum = 186;
 
 fs.readFile(`src/simulation/world-data/data/circlemud/zon/${zonNum}.zon`, "utf-8", (err, data) => {
   if (err) {
---
+++ src/simulation/world-data/libs/zone/writeZonJsonToDb.ts
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const sqlite3 = require("sqlite3").verbose();
+
+try {
+  fs.unlinkSync(`src/databases/imported/zones.db`, (err) => {});
+} catch (error) {}
+
+const db = new sqlite3.Database("src/databases/imported/zones.db");
+
+db.serialize(() => {
+  db.run(`
+  CREATE TABLE IF NOT EXISTS Zones (
+    id INTEGER PRIMARY KEY,
+    vNum INTEGER,
+    data TEXT,
+    created TEXT DEFAULT (datetime('now', 'utc')),
+    lastUpdated TEXT DEFAULT (datetime('now', 'utc'))
+  );
+
+  CREATE INDEX idx_vNum ON Zones (vNum)
+  `);
+
+  fs.readdir("src/simulation/world-data/data/json/zon/", (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      if (file.endsWith(".json")) {
+        fs.readFile(`src/simulation/world-data/data/json/zon/${file}`, "utf8", (err, content) => {
+          if (err) throw err;
+
+          const zone = JSON.parse(content);
+          db.run("INSERT INTO Zones (vNum, data) VALUES (?, ?)", [zone.id, content], (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+    });
+    // db.close();
+  });
+});
