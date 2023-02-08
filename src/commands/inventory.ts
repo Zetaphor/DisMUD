5f9e674d0ff50f37a430dcb03c2a2ae3f224293c
Zetaphor <zetaphor@hey.com>
2023-02-08 14:21:31 -0300

    Inventory command working

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -1,18 +1,26 @@
 import emoji from "../messages/emoji";
 
 export default async function say(worldState, userData, msg) {
-  let playerInventory = await worldState.inventories.getInventory(
-    worldState.db["playerInventories"],
-    userData.discordId
-  );
-  const inventoryKeys = Object.keys(playerInventory);
-  if (!inventoryKeys.length) {
+  let playerInventory = await worldState.inventories.getInventory(worldState.db["playerInventories"], userData.id);
+
+  if (Object.keys(playerInventory).length === 0) {
     userData.user.send(`${emoji.backpack} _You don't have any items in your inventory. Get out and find some loot!_`);
   } else {
+    let inventoryMessage = "";
+    let totalQty = 0;
+    for (const item in playerInventory) {
+      if (!Object.prototype.hasOwnProperty.call(playerInventory, item)) continue;
+      if (playerInventory[item]["qty"] > 1) {
+        inventoryMessage += `(${playerInventory[item]["qty"]}) ${playerInventory[item]["data"].shortDesc}\n`;
+      } else {
+        inventoryMessage += `${playerInventory[item]["data"].shortDesc}\n`;
+      }
+      totalQty += playerInventory[item]["qty"];
+    }
+
     userData.user.send(`
-      ${emoji.backpack} __**Inventory**__ (${inventoryKeys.length}) items\n
-      ${inventoryKeys.map((key) => `${key}: ${playerInventory[key].name}`).join("\n")}
+      ${emoji.backpack} __**INVENTORY**__ _${totalQty} item${totalQty > 1 ? "s" : ""}_\n
+      ${inventoryMessage}
     `);
   }
-  console.log(`${userData.user.username} inventory:`, playerInventory);
 }
