b82d0272d6da0376aab8f5093a68319f0156f63c
Zetaphor <zetaphor@hey.com>
2023-02-09 14:24:59 -0300

    Update playerStats

--- src/simulation/components/Stats.ts
+++ src/simulation/components/Stats.ts
@@ -1,15 +1,34 @@
 import { defineComponent, Types } from "bitecs";
 
-// Used to indicate Player characters
+// Used to define an entities stats
 export const Stats = defineComponent({
   str: Types.i32,
+  strMod: Types.i32,
   dex: Types.i32,
+  dexMod: Types.i32,
   con: Types.i32,
+  conMod: Types.i32,
   int: Types.i32,
+  intMod: Types.i32,
   wis: Types.i32,
+  wisMod: Types.i32,
   cha: Types.i32,
+  chaMod: Types.i32,
   ac: Types.i32,
+  acMod: Types.i32,
   mana: Types.i32,
+  manaMax: Types.i32,
+  manaMod: Types.i32,
+  move: Types.i32,
+  moveMax: Types.i32,
+  moveMod: Types.i32,
+  hitRoll: Types.i32,
+  damRoll: Types.i32,
+  savingHit: Types.i32,
+  savingSpell: Types.i32,
+  hp: Types.i32,
+  hpMax: Types.i32,
+  hpMod: Types.i32,
 });
 
 export default Stats;
---
+++ src/simulation/constants/players.ts
@@ -0,0 +1,16 @@
+export const playerConstants = {
+  MOVE_COSTS: {
+    inside: 1,
+    city: 1,
+    field: 2,
+    forest: 3,
+    hills: 4,
+    mountains: 6,
+    swimming: 4,
+    unswimmable: 1,
+    flying: 1,
+    underwater: 5,
+  },
+};
+
+export default playerConstants;
