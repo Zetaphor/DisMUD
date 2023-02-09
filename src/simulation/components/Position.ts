189287b5753cb932eb6af4c96b5464d5cd927812
Zetaphor <zetaphor@hey.com>
2023-02-09 18:01:43 -0300

    Position needs to be signed int

--- src/simulation/components/Position.ts
+++ src/simulation/components/Position.ts
@@ -1,5 +1,5 @@
 import { defineComponent, Types } from "bitecs";
 
-export const Position = defineComponent({ roomNum: Types.ui32 });
+export const Position = defineComponent({ roomNum: Types.i32 });
 
 export default Position;
