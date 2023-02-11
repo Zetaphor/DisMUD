d60ce91e56a9922021d390ae5bd2141332feed9f
Zetaphor <zetaphor@hey.com>
2023-02-11 20:46:09 -0300

    Reformat player move costs

--- src/simulation/constants/players.ts
+++ src/simulation/constants/players.ts
@@ -1,15 +1,15 @@
 export const playerConstants = {
   MOVE_COSTS: {
-    inside: 1,
-    city: 1,
-    field: 2,
-    forest: 3,
-    hills: 4,
-    mountains: 6,
-    swimming: 4,
-    unswimmable: 1,
-    flying: 1,
-    underwater: 5,
+    0: 1, // Inside
+    1: 1, // City
+    2: 2, // Field
+    3: 3, // Forest
+    4: 4, // Hills
+    5: 6, // Mountains
+    6: 4, // Swimming
+    7: 1, // Unswimmable
+    8: 5, // Underwater
+    9: 1, // Flying
   },
 };
 
