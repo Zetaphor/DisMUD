b7dcb1648e393c062dcf46b6625ca5dea482dd8d
Zetaphor <zetaphor@hey.com>
2023-02-11 12:18:58 -0300

    Cleanup players

--- src/state/players.ts
+++ src/state/players.ts
@@ -2,140 +2,85 @@ import globalConstants from "../simulation/constants/global";
 
 export const players = {
   currentActive: {},
-  login,
-  logout,
-  isActiveId,
-  isActiveDiscordId,
-  getActiveByEntityId,
-  getActiveByDiscordId,
-};
 
-/**
- * Check if the player is currently active
- * @function
- * @param {string} playerId - The player's unique identifier
- * @returns {boolean} - Returns true if the player is active, otherwise false
- */
-function isActiveId(playerId) {
-  if (Object.keys(players.currentActive).indexOf(playerId) !== -1) return true;
-  return false;
-}
-
-/**
- * Check if the player is currently active using a discordId
- * @function
- * @param {string} discordId - The player's Discord ID
- * @returns {boolean} - Returns true if the player is active, otherwise false
- */
-function isActiveDiscordId(discordId) {
-  for (var key in players.currentActive) {
-    if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].discordId === discordId) {
-      return true;
-    }
-  }
-}
-
-/**
- * Get an active player by ID
- * @param eid - The player's entity ID
- * @returns {Object} - The players data
- */
-function getActiveByEntityId(eid) {
-  for (var key in players.currentActive) {
-    if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].eid === eid) {
-      return players.currentActive[key];
-    }
-  }
-}
-
-/**
- * Get an active player by Discord ID
- * @param discordId - The player's Discord ID
- * @returns {Object} - The players data
- */
-function getActiveByDiscordId(discordId) {
-  for (var key in players.currentActive) {
-    if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].discordId === discordId) {
-      return players.currentActive[key];
-    }
-  }
-}
-
-/**
- * Logs a player into the system
- * @async
- * @function
- * @param {object} db - The database object
- * @param {object} simulation - The simulation object
- * @param {object} user - The player's user object
- * @returns {Promise<Object>} - Returns a promise that resolves to a boolean indicating whether the player was just created (true) or already exists (false)
- */
-async function login(worldState, user) {
-  return new Promise<Object>(async (resolve, reject) => {
-    try {
-      let playerData = await worldState.db["players"].methods.getPlayerDataByDiscordId(`k${user.id}`);
-      let playerInventory = {};
-      let newPlayer = false;
-      if (!playerData) {
-        newPlayer = true;
-        console.info(`Creating new player ${user.username} with discordId: ${user.id}`);
-        playerData = await worldState.db["players"].methods.createPlayer({
-          discordId: `k${user.id}`,
-          discordUsername: `${user.username}#${user.discriminator}`,
-          displayName: user.username,
-          roomNum: globalConstants.NEW_USER_ROOMNUM,
-        });
-        console.info(`Creating new player ${user.username}'s inventory`);
-        await worldState.db["playerInventories"].methods.initPlayerInventory(playerData.id);
-      } else {
-        console.info(`Logging in existing player ${user.username} with discordId: ${playerData["discordId"]}`);
-        const loadedInventory = await worldState.db["playerInventories"].methods.getPlayerInventory(
-          BigInt(playerData.id)
-        );
-        if (loadedInventory.inventoryString !== "") playerInventory = JSON.parse(loadedInventory.inventoryString);
-        worldState.inventories.setInventory(playerData.id, playerInventory);
-        await worldState.db["players"].methods.updateLastLogin(BigInt(playerData["id"]));
+  isActiveId(playerId) {
+    if (Object.keys(players.currentActive).indexOf(playerId) !== -1) return true;
+    return false;
+  },
+  isActiveDiscordId(discordId) {
+    for (var key in players.currentActive) {
+      if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].discordId === discordId) {
+        return true;
       }
-      const playerEntityId = await worldState.simulation.createPlayerEntity(
-        playerData.id,
-        globalConstants.NEW_USER_ROOMNUM
-      );
-      playerData["newPlayer"] = newPlayer;
-      playerData["eid"] = playerEntityId;
-      playerData["user"] = user;
-      players["currentActive"][playerData.id] = playerData;
-      resolve(playerData);
-    } catch (err) {
-      console.error(`Error logging in ${user.username}:`, err);
-      reject(err);
     }
-  });
-}
-
-/**
- * Logs a player out of the system
- * @async
- * @function
- * @param {object} players - The players object
- * @param {object} simulation - The simulation object
- * @param {object} user - The player's user object
- * @returns {Promise<void>} - Returns a promise that resolves when the player has been successfully logged out
- */
-async function logout(id, players, simulation, user) {
-  return new Promise<void>((resolve, reject) => {
-    try {
-      simulation.removeWorldEntity(players["currentActive"][id].eid);
-      delete players["currentActive"][user.id];
-      resolve();
-    } catch (error) {
-      console.error(`Error logging out ${user.username}:`, error);
-      reject(error);
+  },
+  getActiveByEntityId(eid) {
+    for (var key in players.currentActive) {
+      if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].eid === eid) {
+        return players.currentActive[key];
+      }
     }
-  });
-}
-
-function initPlayerStats() {
-  return {};
-}
+  },
+  getActiveByDiscordId(discordId) {
+    for (var key in players.currentActive) {
+      if (players.currentActive.hasOwnProperty(key) && players.currentActive[key].discordId === discordId) {
+        return players.currentActive[key];
+      }
+    }
+  },
+  login(worldState, user) {
+    return new Promise<Object>(async (resolve, reject) => {
+      try {
+        let playerData = await worldState.db["players"].methods.getPlayerDataByDiscordId(`k${user.id}`);
+        let playerInventory = {};
+        let newPlayer = false;
+        if (!playerData) {
+          newPlayer = true;
+          console.info(`Creating new player ${user.username} with discordId: ${user.id}`);
+          playerData = await worldState.db["players"].methods.createPlayer({
+            discordId: `k${user.id}`,
+            discordUsername: `${user.username}#${user.discriminator}`,
+            displayName: user.username,
+            roomNum: globalConstants.NEW_USER_ROOMNUM,
+          });
+          console.info(`Creating new player ${user.username}'s inventory`);
+          await worldState.db["playerInventories"].methods.initPlayerInventory(playerData.id);
+        } else {
+          console.info(`Logging in existing player ${user.username} with discordId: ${playerData["discordId"]}`);
+          const loadedInventory = await worldState.db["playerInventories"].methods.getPlayerInventory(
+            BigInt(playerData.id)
+          );
+          if (loadedInventory.inventoryString !== "") playerInventory = JSON.parse(loadedInventory.inventoryString);
+          worldState.inventories.setInventory(playerData.id, playerInventory);
+          await worldState.db["players"].methods.updateLastLogin(BigInt(playerData["id"]));
+        }
+        const playerEntityId = await worldState.simulation.createPlayerEntity(
+          playerData.id,
+          globalConstants.NEW_USER_ROOMNUM
+        );
+        playerData["newPlayer"] = newPlayer;
+        playerData["eid"] = playerEntityId;
+        playerData["user"] = user;
+        players["currentActive"][playerData.id] = playerData;
+        resolve(playerData);
+      } catch (err) {
+        console.error(`Error logging in ${user.username}:`, err);
+        reject(err);
+      }
+    });
+  },
+  logout(id, players, simulation, user) {
+    return new Promise<void>((resolve, reject) => {
+      try {
+        simulation.removeWorldEntity(players["currentActive"][id].eid);
+        delete players["currentActive"][user.id];
+        resolve();
+      } catch (error) {
+        console.error(`Error logging out ${user.username}:`, error);
+        reject(error);
+      }
+    });
+  },
+};
 
 export default players;
