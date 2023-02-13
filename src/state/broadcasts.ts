0e7a6397e92e13d2fd5d5c6d24c57bba6fdcf4a1
Zetaphor <zetaphor@hey.com>
2023-02-13 16:45:36 -0300

    Broadcasts using sendMessage

--- src/state/broadcasts.ts
+++ src/state/broadcasts.ts
@@ -6,7 +6,7 @@ export const broadcasts = {
         for (let i = 0; i < roomPlayers.length; i++) {
           if (roomPlayers[i] === senderId && !localBroadcast) continue;
           const player = worldState.players.getActiveByEntityId(roomPlayers[i]);
-          player.user.send(message);
+          player.sendMessage(player.user, message);
         }
         resolve();
       } catch (err) {
@@ -19,7 +19,7 @@ export const broadcasts = {
     return new Promise<void>((resolve, reject) => {
       try {
         const player = worldState.players.getActiveByEntityId(id);
-        player.user.send(message);
+        player.sendMessage(player.user, message);
         resolve();
       } catch (err) {
         console.error(`Error sending message to player ${id}: ${err}`);
@@ -32,7 +32,7 @@ export const broadcasts = {
       try {
         const worldPlayers = Object.values(worldState.players.currentActive);
         for (let i = 0; i < worldPlayers.length; i++) {
-          worldPlayers[i]["user"].send(message);
+          worldPlayers[i]["sendMessage"](worldPlayers[i]["user"], message);
         }
         resolve();
       } catch (err) {
