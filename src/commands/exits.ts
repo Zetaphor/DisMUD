817fe292f87f37647102808fc523fefcc4d89d01
Zetaphor <zetaphor@hey.com>
2023-02-13 15:24:50 -0300

    Check for valid exits

--- src/commands/exits.ts
+++ src/commands/exits.ts
@@ -5,10 +5,12 @@ export default async function move(worldState, userData, msg) {
     const roomExits = Object.keys(
       await worldState.rooms.getEntityRoomExits(worldState.db["rooms"], worldState.simulation.world, userData.eid)
     );
-    userData.sendMessage(
-      userData.user,
-      `${emoji.directions} _You can see an exit in the following directions:_ **${roomExits.join(", ")}**`
-    );
+    if (!roomExits.length) userData.sendMessage(userData.user, `${emoji.directions} _You don't see an exit anywhere!_`);
+    else
+      userData.sendMessage(
+        userData.user,
+        `${emoji.directions} _You can see an exit in the following directions:_ **${roomExits.join(", ")}**`
+      );
   } catch (err) {
     console.error(`Error using move ${msg}: ${err}`);
     userData.sendMessage(userData.user, `${emoji.error} _Something went wrong!_`);
