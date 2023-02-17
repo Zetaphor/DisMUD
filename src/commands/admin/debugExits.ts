e81f7c61a093021b446a462f2f405f45f70b0a3a
Zetaphor <zetaphor@hey.com>
2023-02-17 20:56:23 -0300

    Fix command formatting

--- src/commands/admin/debugExits.ts
+++ src/commands/admin/debugExits.ts
@@ -5,7 +5,7 @@ export default async function debugExits(worldState, userData, msg) {
     const roomNum = worldState.rooms.getEntityRoomNum(worldState.simulation.world, userData.eid);
     const roomExits = await worldState.rooms.getRoomExits(worldState.db["rooms"], roomNum);
     console.info(`\nDebugging exits for #${roomNum}`, roomExits, "\n");
-    userData.sendMessage(userData.user, `${emoji.check} _Room exit data sent to console.__`);
+    userData.sendMessage(userData.user, `${emoji.check} _Room exit data sent to console._`);
   } catch (err) {
     console.error(`Error using admin debugExits ${msg}: ${err}`);
     userData.sendMessage(userData.user, `${emoji.error} _Something went wrong!_`);
